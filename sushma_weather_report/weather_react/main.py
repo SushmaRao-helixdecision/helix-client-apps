a44d42c7760bbd6fa1c32753b659735a37765a5d
SushmaRao-helixdecision <sushma.rao@helixdecision.com>
2025-09-11 11:03:20 -0500

    Initial code generation

---
+++ sushma_weather_report/weather_react/main.py
@@ -0,0 +1,22 @@
+            Here is a JSON response with the requested information:
+            {
+                "main_file": {
+                    "filename": "index.js",
+                    "content": "import React from 'react';\n\nfunction Weather() {\n  return ( <div>Weather</div> );\n}\n\nexport default Weather;"
+                },
+                "additional_files": [
+                    {
+                        "filename": "package.json",
+                        "content": "{ \"name\": \"weather-react\", \"version\": \"1.0.0\", \"description\": \"A simple weather app in React\", \"main\": \"index.js\", \"scripts\": { \"start\": \"node index.js\" }, \"author\": \"Your Name\", \"license\": \"MIT\" }"
+                    },
+                    {
+                        "filename": "style.css",
+                        "content": ".weather-app {\n  font-family: Arial, sans-serif;\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  justify-content: center;\n  height: 100vh;\n}\n\n.weather-card {\n  box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.2);\n  padding: 20px;\n  margin: 20px;\n  background-color: #f7f7f7;\n}"
+                    }
+                ],
+                "file_structure": {
+                    "type": "react",
+                    "main_language": "jsx",
+                    "files_count": 3
+                }
+            }
